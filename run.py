b5087a1c13a2da198839ea1ae71c8eb28d112ae8
Roshall <15504658671@163.com>
2024-04-04 11:05:53 +0800

    update run script

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from itertools import takewhile
+from functools import partial
 from time import perf_counter as now
 
 from configs import cfg
@@ -6,22 +6,38 @@ from indices.builder import build_tempo_spatial_index
 from search.base import base_search
 from search.baseline.search_methods import sliding_framework
 from search.one_pass import one_pass_search
-from utilities import dataset
 from utilities.box2D import Box2D
-from utilities.config import config
-from utilities.data_preprocessing import traj_data, gen_border, view_field
+from utilities.data_preprocessing import traj_data
 from utilities.dataset import load_yolo_for
 
 
-def get_index(file_name, cfg, higher_bound):
-    data, cols, cls_map = dataset.load_yolo_for(file_name)
-    trajs = traj_data(data, cols, 5, cls_map, scale=1)
+def get_index(frame_info, cfg):
+    trajs = traj_data(*frame_info, cfg.DATA.STRIDE, scale=cfg.DATA.SCALE)
     # broders = gen_border(trajs.bbox, 16, 11)
-    broders = gen_border(view_field(data, *cols[-2:]), 8, 6)
-    config.gird_border = broders
-    trajs = takewhile(lambda t: t[1] < higher_bound * 1.2, trajs)
     temp_spt = build_tempo_spatial_index(trajs, cfg)
     return temp_spt
+
+
+def init(query_type):
+    match query_type:
+        case ('index', mtd):
+            frame_info = load_yolo_for(filename)
+            frame_info[0].query("3600 <= fid <= 18000", inplace=True)
+            data = get_index(frame_info, cfg)
+            match mtd:
+                case 'one_pass':
+                    search_mtd = one_pass_search
+                case 'base':
+                    cfg.merge_from_file('configs/region_base.yml')
+                    search_mtd = base_search
+                case _:
+                    raise ValueError(f'No index based {mtd} found')
+        case ('sliding', mtd):
+            data, _, _ = load_yolo_for(filename)
+            search_mtd = partial(sliding_framework, method=mtd)
+        case _:
+            raise ValueError(f'wrong query type: {cfg.QUERY}')
+    return partial(search_mtd, data)
 
 
 def query():
@@ -32,24 +48,11 @@ def query():
         (5, 100),  # duration
         (0, interval_bound),  # interval
     ]
-    query_content[0] = Box2D((700, 1000, 427, 569))
-    match cfg.QUERY:
-        case 'index_one_pass':
-            search_mtd = one_pass_search
-        case 'index_base':
-            cfg.merge_from_file('configs/region_base.yml')
-            search_mtd = base_search
-        case 'sliding_base':
-            search_mtd = sliding_framework
-        case _:
-            raise ValueError(f'wrong query type: {cfg.QUERY}')
-    if cfg.QUERY.startswith('index'):
-        data = get_index(filename, cfg, interval_bound)
-    else:
-        data, _, _ = load_yolo_for(filename)
+    # query_content[0] = Box2D((700, 1000, 427, 569))
+    search_mtd = init(('index', 'one_pass'))
     count = 0
     start = now()
-    for _ in search_mtd(data, *query_content):
+    for _ in search_mtd(*query_content):
         count += 1
     end = now()
     print(cfg.QUERY, 'result count:', count, 'using', end - start, 's')
@@ -64,17 +67,17 @@ def find_bug():
         (60*60, interval_bound),  # interval
     ]
     query_content[0] = Box2D((700, 1000, 427, 569))
-    data = get_index(filename, cfg, interval_bound)
-    one = list(one_pass_search(data, *query_content))
-    one = set((frozenset(ids), (s, e)) for ids, s, e in one_pass_search(data, *query_content))
-    cfg.merge_from_file('configs/region_base.yml')
-    data = get_index(filename, cfg, interval_bound)
-    two = set((frozenset(comv.labels), tuple(comv.interval)) for comv in base_search(data, *query_content))
-    print('one_pass_res_num:', len(one))
-    # print('one_pass_res:', one)
-    print('two_pass_res_num:', len(two))
-    print('In one_pass but not  base: ', one - two)
-    print('In base but not one_pass: ', two - one)
+
+    canary_mtd = init(('index', 'one_pass'))
+    one = set((frozenset(ids), (s, e)) for ids, s, e in canary_mtd(*query_content))
+    test_mtd = init(('sliding', 'base'))
+    two = set((frozenset(ids), (s, e)) for ids, s, e in test_mtd(*query_content))
+
+    print('canary_res_num:', len(one))
+    print('one_pass_res:', one)
+    print('test_res_num:', len(two))
+    print('In canary but not test: ', one - two)
+    print('In test but not canary: ', two - one)
 
 
 filename = '../resource/dataset/traj_taipei_0412.pkl'
