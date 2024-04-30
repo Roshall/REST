b624b775fff7d97b3a52ce8d7c44cfaa6ce5bff8
Roshall <15504658671@163.com>
2024-04-30 16:17:10 +0800

    add external index border config

--- configs/defaults.py
+++ configs/defaults.py
@@ -16,16 +16,18 @@ _C.INDEX.REGION.COARSE = False
 _C.INDEX.REGION.TYPE = 'grid'
 
 _C.INDEX.REGION.GRID = CN()
-_C.INDEX.REGION.GRID.SPACE = (8, 6)
-_C.INDEX.REGION.GRID.AREA = (0, 1920, 0, 1080)
+_C.INDEX.REGION.GRID.SPACE = (16, 12)
+
+_C.INDEX.CONFIG_PATH = '/home/lg/VDBM/spatiotemporal/regional_tempo_spatial_query/meta'
 
 _C.DATA = CN()
+_C.DATA.PATH = '/home/lg/VDBM/spatiotemporal/regional_tempo_spatial_query/test'
 _C.DATA.SCALE = 1
 _C.DATA.STRIDE = 1
 
 _C.BOX_NP = True
 
-_C.QUERY = ('index', 'one_pass')
+# _C.QUERY = ('index', 'one_pass')
 # _C.QUERY = ('index', 'base')
 # _C.QUERY = ('sliding', 'base')
 # _C.QUERY = ('sliding', 'state')
