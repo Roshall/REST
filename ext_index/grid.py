a741f8322a5d7c16acde1736ab14133eb03bff77
Roshall <15504658671@163.com>
2024-04-30 16:17:34 +0800

    add grid compatible with external grid

---
+++ ext_index/grid.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class Grid:
+    def __init__(self, border_stride):
+        border_stride = np.array(border_stride)
+        self.lower_border = np.array(border_stride[[0, 3]])
+        self.stride = np.array(border_stride[[2, 5]])
+        box = border_stride[[0, 1, 3, 4]].reshape(-1, 2)
+        box = (box[:, 1] - box[:, 0]) // np.asarray(self.stride) + 1
+        self.size = box[0] * box[1]
+        self.col_num = box[0]
+
+    def index(self, point):
+        pos = (point - self.lower_border)/self.stride
+        pos = pos[:, 1] * self.col_num + pos[:, 0]
+        if (pos >= self.size).any():
+            raise ValueError(f"points outside grid")
+        return pos
