718369934d65f72d43150e651550489f7655c3a3
Roshall <lg6zi7ran6@gmail.com>
2026-06-12 11:07:12 +0800

    remove numba

--- my_numb/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-MY_NUMBA_ROOT = os.path.abspath(os.path.dirname(__file__))
-os.environ['NUMBA_CACHE_DIR'] = os.path.join(MY_NUMBA_ROOT, 'code_cache')
--- my_numb/cxx_store/__init__.py
+++
@@ -1,5 +0,0 @@
-import os.path
-
-LIB_DIR = os.path.abspath(os.path.dirname(__file__))
-LIB_NAME = 'index'
-HEADER = 'index_connector.h'
