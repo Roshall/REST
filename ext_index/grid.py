5950d355dbe1fe588f9744967ea97cca4262ed49
Roshall <lg6zi7ran6@gmail.com>
2026-06-22 17:58:02 +0800

    refactor(ext_index/grid): remove unnecessary np.asarray cast
    Simplify the code by removing the redundant numpy array cast on self.stride, since self.stride is already a numpy array when initialized.

--- ext_index/grid.py
+++ ext_index/grid.py
@@ -7,7 +7,7 @@ class Grid:
         self.lower_border = np.array(border_stride[[0, 3]])
         self.stride = np.array(border_stride[[2, 5]])
         box = border_stride[[0, 1, 3, 4]].reshape(-1, 2)
-        box = (box[:, 1] - box[:, 0]) // np.asarray(self.stride) + 1
+        box = (box[:, 1] - box[:, 0]) // self.stride + 1
         self.size = box[0] * box[1]
         self.col_num = box[0]
 
