3b8af87577f4bbc631e5fc2d1555af1c2e7dd842
Roshall <lg6zi7ran6@gmail.com>
2026-06-23 13:28:21 +0800

    refactor(dataset): improve code readability and structure in load functions

--- utilities/dataset.py
+++ utilities/dataset.py
@@ -4,28 +4,56 @@ import pandas as pd
 
 
 def load_rounD(path, track_id):
-    with ZipFile(f'{path}/rounD.zip') as zf:
-        prefix = f'rounD/data/{track_id}_tracks'
-        with zf.open(f'{prefix}Meta.csv') as f:
-            meta = pd.read_csv(f, index_col='trackId', usecols=['trackId', 'class'])
+    with ZipFile(f"{path}/rounD.zip") as zf:
+        prefix = f"rounD/data/{track_id}_tracks"
+        with zf.open(f"{prefix}Meta.csv") as f:
+            meta = pd.read_csv(f, index_col="trackId", usecols=["trackId", "class"])
 
-        with zf.open(f'{prefix}.csv') as f:
+        with zf.open(f"{prefix}.csv") as f:
             data = pd.read_csv(f)
-    label = ('car', 'truck', 'van', 'bus', 'trailer', 'bicycle', 'motorcycle', 'pedestrian')
-    class_map = meta['class'].replace({l: i for i, l in enumerate(label)})
+    label = (
+        "car",
+        "truck",
+        "van",
+        "bus",
+        "trailer",
+        "bicycle",
+        "motorcycle",
+        "pedestrian",
+    )
+    class_map = meta["class"].replace({l: i for i, l in enumerate(label)})
     return class_map, data
 
 
-def load_fake():
+def load_fake(return_cls_m=True):
     import os
-    file_name = os.path.join(os.path.dirname(__file__), '/home/lg/VDBM/spatiotemporal/resource/dataset/fake_tracks.csv')
+
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        "/home/lg/VDBM/spatiotemporal/resource/dataset/fake_tracks.csv",
+    )
     data = pd.read_csv(file_name)
-    columns = ['oid', 'fid', 'x', 'y']
-    data[['x', 'y']] *= 100
-    return data, columns, data[['oid', 'cls']].drop_duplicates('oid').set_index('oid')['cls']
+    columns = ["oid", "fid", "x", "y"]
+    data[["x", "y"]] *= 100
+    data = data.astype({"x": "int16", "y": "int16"})
+    if return_cls_m:
+        return (
+            data,
+            columns,
+            data[["oid", "cls"]].drop_duplicates("oid").set_index("oid")["cls"],
+        )
+    else:
+        return data, columns
 
 
-def load_yolo_for(filename):
-    columns = ['oid', 'fid', 'x', 'y']
-    data = pd.read_pickle(filename)
-    return data, columns, data[['oid', 'cls']].drop_duplicates('oid').set_index('oid')['cls']
+def load_yolo_for(filename, return_cls_m=True):
+    columns = ["oid", "fid", "x", "y"]
+    data = pd.read_parquet(filename)
+    if return_cls_m:
+        return (
+            data,
+            columns,
+            data[["oid", "cls"]].drop_duplicates("oid").set_index("oid")["cls"],
+        )
+    else:
+        return data, columns
